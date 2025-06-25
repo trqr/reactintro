856c1ef95f5d3c50d87ad3ce66d84492dbce39f1
TrqR <tristanqr@gmail.com>
2025-06-25 22:46:12 +0200

    Code promo en cours

--- src/components/CheckoutCart.tsx
+++ src/components/CheckoutCart.tsx
@@ -1,11 +1,21 @@
 import {useCart} from "../context/useCart.tsx";
 import type {cartItem} from "../context/CartProvider.tsx";
-import { Box, Button, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
+import {Box, Button, FormControl, ListItem, ListItemAvatar, ListItemText, TextField} from "@mui/material";
 
 
 const CheckoutCart = () => {
     const { cart } = useCart();
     const { removeFromCart, getCartTotal} = useCart();
+    const [promoCode, setPromoCode] = useState<string>('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPromoCode(e.target.value);
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setPromoCode(promoCode);
+    }
 
     return (
         <>
@@ -47,6 +57,15 @@ const CheckoutCart = () => {
                             primary={"Delivery :"}/>
                     </ListItem>
                     <ListItem className={"line-checkout"}
+                              secondaryAction={
+                                  <ListItemText
+                                      primary={"0 €"}/>
+                              }>
+                        <ListItemText
+                            className={"checkout-text-subtotal list-checkout"}
+                            primary={"CodePromo :"}/>
+                    </ListItem>
+                    <ListItem className={"line-checkout"}
                         secondaryAction={
                             <ListItemText
                                 primary={getCartTotal().toFixed(2) + " €"}/>
@@ -56,6 +75,31 @@ const CheckoutCart = () => {
                             primary={"Total :"}/>
                     </ListItem>
                 </Box>
+                <form onSubmit={handleSubmit}>
+                    <FormControl fullWidth={true} sx={{marginTop: 0}}>
+                        <Box sx={{display: "flex", justifyContent: "space-between", alignContent: "center", alignItems: "center"}}>
+                            <TextField
+                                id="filled-basic"
+                                label="Code Promo"
+                                variant="filled"
+                                size={"small"}
+                                value={promoCode}
+                                onChange={handleChange}
+                            />
+                            <Button
+                                sx={{marginBottom: "20px", width: "200px"}}
+                                type="submit"
+                                variant="contained"
+                                size="large"
+                                color="primary"
+                                style={{marginTop: '1rem'}}
+                            >
+                                Appliquer
+                            </Button>
+                        </Box>
+                    </FormControl>
+                </form>
+
             </div>
         </>
     )

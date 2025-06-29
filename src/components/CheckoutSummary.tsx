9018fcfdac477df259f22e828e40609bbf5134b6
TrqR <tristanqr@gmail.com>
2025-06-29 08:00:36 +0200

    add/remove from fav updated with backend

--- src/components/CheckoutSummary.tsx
+++ src/components/CheckoutSummary.tsx
@@ -1,9 +1,24 @@
-import {Box} from "@mui/material";
+import {Box, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup} from "@mui/material";
 
 const CheckoutSummary = () => {
   return (
       <>
-          <h2 className={"title checkout-summary-title"}>Finish your order</h2>
+          <Box sx={{display: "flex", flexDirection: "column", gap: 2, border: "1px solid #dfdede", width: "500px", padding:}}>
+              <h2 className={"title checkout-summary-title"}>Finish your order</h2>
+
+              <FormControl>
+                  <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+                  <RadioGroup
+                      aria-labelledby="demo-radio-buttons-group-label"
+                      defaultValue="female"
+                      name="radio-buttons-group"
+                  >
+                      <FormControlLabel value="female" control={<Radio/>} label="Female"/>
+                      <FormControlLabel value="male" control={<Radio/>} label="Male"/>
+                      <FormControlLabel value="other" control={<Radio/>} label="Other"/>
+                  </RadioGroup>
+              </FormControl>
+          </Box>
       </>
   );
 };
